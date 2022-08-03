9077ec34a41138bae0e34d13c7048cba10d68b50
aris22002-bitbucket <aris.22002.priyanto@gmail.com>
2022-08-03 21:07:42 +0700

    aaa

--- pages/user/wishlist/index.vue
+++ pages/user/wishlist/index.vue
@@ -3,7 +3,7 @@
     <h3 class="title">{{ pageTitle }}</h3>
     <div class="columns is-centered is-multiline">
       <div class="card column is-one-quarter" v-for="product in productsInWishlist" :key="product.id">
-        <VmProductsList :product="product"></VmProductsList>
+        <!-- <VmProductsList :product="product"></VmProductsList> -->
       </div>
       <!-- <div class="section" v-if="productsInWishlist.length === 0">
         <p>{{ noProductLabel }}</p>
